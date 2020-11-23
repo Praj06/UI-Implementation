51d7be372308024c239ed88068fd0327d1a66687
Prajakta thakur <11013754@stud.hochschule-heidelberg.de>
2020-11-23 20:57:52 +0100

    Update Challenge1.js

--- Challenge1.js
+++ Challenge1.js
@@ -1,6 +1,22 @@
-function calculateLPN(dob) {
-    return [...dob.split('-')].map(v=>v.split('').reduce((a,b)=>a+b*1,0))
-   .map(v=>v.toString().split('').reduce((a,b)=>a+b*1,0)).reduce((a,b)=>a+b*1,0).toString().split('').reduce((a,b)=>a+b*1,0)
-   .toString().split('').reduce((a,b)=>a+b*1,0)
-  }
-  console.log(calculateLPN("1994-06-01"));
+function calLPN(dob) {
+        let res = dob.split('-');
+        console.log(res);
+        let y = res.join('');
+        console.log(y);
+        let sumDOB = parseInt(y[0])+parseInt(y[1])+parseInt(y[2])+parseInt(y[3])+
+                  parseInt(y[4])+parseInt(y[5])+parseInt(y[6])+parseInt(y[7]);
+                
+        let num = parseInt(sumDOB);
+        let calculate=0;
+        while(num > 0) {
+            let remainder = num%10;
+            calculate += remainder;
+            num  = Math.floor(num / 10);
+            if(calculate > 9) {
+                num = calculate;
+                calculate = 0;
+            }
+        }
+        console.log("Life Path Number: "+calculate);
+    }
+  calLPN("1994-06-01");
