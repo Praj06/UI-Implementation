5a5d17734664b79307da93b013a378ca6d6b4ccf
Prajakta thakur <11013754@stud.hochschule-heidelberg.de>
2020-11-23 17:04:13 +0100

    Add files via upload
    Challenge 4

---
+++ Challenge4.js
@@ -0,0 +1,10 @@
+function calculate(sp, di){
+    if (sp < 0){
+            t = 0;
+            console.log(t);
+            } else {
+            t = di / sp ;
+            console.log(t);
+         }
+}
+calculate(5,400);
